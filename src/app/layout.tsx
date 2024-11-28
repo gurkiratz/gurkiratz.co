2234f82af9091f4ba3030521546dd489c03603ff
Gurkirat Singh <94279121+gurkiratz@users.noreply.github.com>
2024-11-28 12:50:27 -0500

    Update metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
       'Gurkirat Singh - Full Stack Developer and student',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Gurkirat, a Software Engineering student at Humber Polytechnic, Toronto.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
