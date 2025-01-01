97810db9d13870d92f7d7d1f515c467b83fb28e4
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2024-12-31 21:36:04 -0500

    update remotePatterns and add redirects file

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,28 @@ import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 import { withPayload } from '@payloadcms/next/withPayload'
+import redirects from './redirects.mjs'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined ||
+    process.env.NEXT_PUBLIC_SERVER_URL ||
+    'http://localhost:3000' ||
+    'http://localhost:3001'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   images: {
     remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+        }
+      }),
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
@@ -20,20 +36,9 @@ const nextConfig = {
         port: '',
         search: '',
       },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        search: '',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-        search: '',
-      },
     ],
   },
+  redirects,
 }
 
 const withMDX = nextMDX({
