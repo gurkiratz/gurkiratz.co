2a30ccca48be4f1f28c6d2c0a992ad6b1a436799
gurkiratz <singh.gurkirat2707@gmail.com>
2025-05-22 20:20:30 -0400

    fix build errors and upgrade payload

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,8 +2,5 @@ import { type MDXComponents } from 'mdx/types'
 import Image, { type ImageProps } from 'next/image'
 
 export function useMDXComponents(components: MDXComponents) {
-  return {
-    ...components,
-    Image: (props: ImageProps) => <Image {...props} />,
-  }
+  return { ...components, Image: (props: ImageProps) => <Image {...props} /> }
 }
